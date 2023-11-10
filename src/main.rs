da5c228092b9ed57d2697aba892c369b447f270d
Sergei Patrikeev <serejke.best@gmail.com>
2023-11-10 16:07:11 +0000

    feat: read the solver key.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use ethers::{
+    signers::{LocalWallet, Signer}
+};
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// Private key for sending txs.
+    #[arg(long)]
+    pub private_key: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let wallet: LocalWallet = args
+        .private_key
+        .parse::<LocalWallet>()
+        .unwrap();
+
+    let address = wallet.address();
+    println!("Solver address: {}", address);
 }
