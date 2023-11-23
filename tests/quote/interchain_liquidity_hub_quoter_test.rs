b739a7b6e849835e439227cd31bcd5e9dbe604cc
Sergei Patrikeev <serejke.best@gmail.com>
2023-11-23 10:21:27 +0000

    fix: test range.

--- tests/quote/interchain_liquidity_hub_quoter_test.rs
+++ tests/quote/interchain_liquidity_hub_quoter_test.rs
@@ -1,10 +1,10 @@
+use std::ops::Range;
 use std::sync::Arc;
 
 use anyhow::Result;
 use ethers::types::U256;
 
 use khalani_solver::config::chain::KHALANI_CHAIN_ID;
-use khalani_solver::inventory::amount::Amount;
 use khalani_solver::inventory::inventory::Inventory;
 use khalani_solver::quote::interchain_liquidity_hub_quoter::InterchainLiquidityHubQuoter;
 use khalani_solver::types::swap_intent::SwapIntent;
@@ -38,10 +38,8 @@ async fn test_interchain_liquidity_hub_quoter() -> Result<()> {
     let usdt_fuji = inventory
         .find_token_by_symbol("USDT.fuji".into(), KHALANI_CHAIN_ID)
         .unwrap();
-    let kai = inventory
-        .find_token_by_symbol("KAI".into(), KHALANI_CHAIN_ID)
-        .unwrap();
 
+    let source_amount = U256::from_str_radix("1000", 10).unwrap();
     let intent_get_usdc_sepolia = SwapIntent {
         intent_id: Default::default(),
         author: Default::default(),
@@ -50,7 +48,7 @@ async fn test_interchain_liquidity_hub_quoter() -> Result<()> {
         destination_chain_id: Default::default(),
         source_token: usdc_sepolia.address,
         destination_token: usdc_sepolia.address,
-        source_amount: U256::from_str_radix("1000", 10).unwrap(),
+        source_amount,
         source_permit_2: Default::default(),
     };
 
@@ -72,34 +70,31 @@ async fn test_interchain_liquidity_hub_quoter() -> Result<()> {
         ..intent_get_usdc_sepolia.clone()
     };
 
-    let amounts: Vec<Amount> = vec![
+    let amounts: Vec<U256> = vec![
         quoter.quote_intent(intent_get_usdc_sepolia).await?,
         quoter.quote_intent(intent_get_usdt_sepolia).await?,
         quoter.quote_intent(intent_get_usdc_fuji).await?,
         quoter.quote_intent(intent_get_usdt_fuji).await?,
     ]
     .iter()
-    .map(|quoted_intent| quoted_intent.kai_amount.clone())
+    .map(|quoted_intent| quoted_intent.kai_amount.base_units.clone())
     .collect();
-    let expected_ranges: Vec<Vec<i64>> = vec![
-        vec![990005080912622, 997705080912622],
-        vec![996965540372730, 999965540372730],
-        vec![1000640051552452, 1009640051552452],
-        vec![990593362546388, 999593362546388],
-    ];
 
-    println!("quoted amounts: {:?}", amounts);
+    // From 990 to 1010 per 1.
+    let expected_range = U256::from(990000000000000u64)..U256::from(1010000000000000u64);
 
-    for (index, amount) in amounts.iter().enumerate() {
-        assert!(
-            *amount >= Amount::from_base_units(expected_ranges[index][0].into(), kai.decimals),
-            "quoted amount is lower than minimum expected amount"
-        );
-        assert!(
-            *amount <= Amount::from_base_units(expected_ranges[index][1].into(), kai.decimals),
-            "quoted amount is higher than maximum expected amount"
-        );
+    for (amount) in amounts.iter() {
+        assert_in_range(*amount, expected_range.clone());
     }
 
     Ok(())
+}
+
+fn assert_in_range(value: U256, range: Range<U256>) {
+    assert!(
+        range.contains(&value),
+        "Value {} is not within the expected range {:?}",
+        value,
+        range
+    );
 }
