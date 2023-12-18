dc1d242ab542cbe90ab0a0bc639dd27132cac535
Konrad Bieniek <konraddbieniekk@gmail.com>
2023-12-18 09:23:00 +0100

    chore: run engine logic

--- spoke-chain-caller/src/main.rs
+++ spoke-chain-caller/src/main.rs
@@ -9,6 +9,8 @@ use tracing::info;
 pub mod types;
 pub mod workflow;
 
+use workflow::engine::configure_engine;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     configure_logs();
@@ -21,5 +23,19 @@ async fn main() -> Result<()> {
     let inventory = Inventory::new(config.clone(), connector.clone()).await?;
     let inventory = Arc::new(inventory);
 
+    // Set up engine.
+    let engine: Engine<Event, Action> = configure_engine(&config, connector.clone(), inventory);
+
+    // Start engine.
+    run_engine(engine).await;
+ 
     Ok(())
+}
+
+async fn run_engine(engine: Engine<Event, Action>) {
+    if let Ok(mut set) = engine.run().await {
+        while let Some(res) = set.join_next().await {
+            info!("Result: {:?}", res);
+        }
+    }
 }
