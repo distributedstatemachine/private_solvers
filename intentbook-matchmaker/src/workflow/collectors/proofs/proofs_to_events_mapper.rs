6f212a90448687c6a15290f784651ffac782df35
Sergei Patrikeev <serejke.best@gmail.com>
2023-12-20 11:27:34 +0000

    fix: map "spoke chain call" proof.

--- intentbook-matchmaker/src/workflow/collectors/proofs/proofs_to_events_mapper.rs
+++ intentbook-matchmaker/src/workflow/collectors/proofs/proofs_to_events_mapper.rs
@@ -3,11 +3,13 @@ use ethers::utils::keccak256;
 use tracing::debug;
 
 use crate::types::intent::Intent;
+use crate::types::intent_bid::IntentBid;
 use crate::types::proof_id::ProofId;
 use crate::workflow::event::Event;
 use crate::workflow::state::IntentState;
 use solver_common::types::intent_id::IntentId;
 use spoke_chain_caller::types::spoke_chain_call::SpokeChainCall;
+use spoke_chain_caller::types::spoke_chain_call_bid::SpokeChainCallBid;
 
 #[derive(Default)]
 pub struct ProofsToEventsMapper {}
@@ -19,14 +21,19 @@ impl ProofsToEventsMapper {
         proof_id: ProofId,
     ) -> Option<Event> {
         for intent_state in all_intents {
-            if let Intent::SpokeChainCall(spoke_chain_caller) = &intent_state.intent {
-                let event = self.try_map_spoke_chain_call(
-                    proof_id,
-                    intent_state.intent.id(),
-                    spoke_chain_caller,
-                );
-                if event.is_some() {
-                    return event;
+            if let Intent::SpokeChainCall(spoke_chain_call) = &intent_state.intent {
+                if let Some(IntentBid::SpokeChainCallBid(spoke_chain_call_bid)) =
+                    &intent_state.matched_bid
+                {
+                    let event = self.try_map_spoke_chain_call(
+                        proof_id,
+                        intent_state.intent.id(),
+                        spoke_chain_call,
+                        spoke_chain_call_bid,
+                    );
+                    if event.is_some() {
+                        return event;
+                    }
                 }
             }
         }
@@ -38,9 +45,11 @@ impl ProofsToEventsMapper {
         proof_id: ProofId,
         intent_id: IntentId,
         spoke_chain_call: &SpokeChainCall,
+        spoke_chain_call_bid: &SpokeChainCallBid,
     ) -> Option<Event> {
         let spoke_chain_call = encode_packed(&[
             AbiToken::String(String::from("SpokeCalled")),
+            AbiToken::Address(spoke_chain_call_bid.caller),
             AbiToken::FixedBytes(Vec::from(intent_id.as_bytes())),
             AbiToken::Address(spoke_chain_call.contract_to_call),
             AbiToken::Bytes(spoke_chain_call.call_data.to_vec()),
