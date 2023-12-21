eda8742c6edba00f2979044c56674407a2ef11d5
Sergei Patrikeev <serejke.best@gmail.com>
2023-12-21 17:31:20 +0000

    fix: calculate intent bid ID.

--- intentbook-matchmaker/src/types/intent_bid.rs
+++ intentbook-matchmaker/src/types/intent_bid.rs
@@ -1,9 +1,11 @@
+use ethers::abi::{encode_packed, Token};
+use ethers::utils::keccak256;
+
+use solver_common::types::intent_id::{IntentBidId, IntentId};
+
 use crate::types::limit_order_intent_bid::LimitOrderIntentBid;
 use crate::types::spoke_chain_call_bid::SpokeChainCallBid;
 use crate::types::swap_intent_bid::SwapIntentBid;
-use ethers::abi::{encode_packed, Token};
-use ethers::utils::keccak256;
-use solver_common::types::intent_id::{IntentBidId, IntentId};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum IntentBid {
@@ -44,7 +46,6 @@ pub fn calculate_intent_bid_id(
     intent_bid: bindings_khalani::base_intent_book::IntentBid,
 ) -> IntentBidId {
     keccak256(
-        // TODO[solidity]: ensure this encoding is exactly what Solidity returns (write a test).
         encode_packed(&[
             Token::FixedBytes(Vec::from(intent_bid.intent_id)),
             Token::Bytes(intent_bid.bid.to_vec()),
@@ -52,4 +53,38 @@ pub fn calculate_intent_bid_id(
         .unwrap(),
     )
     .into()
+}
+
+#[cfg(test)]
+mod tests {
+    use ethers::abi::AbiDecode;
+    use ethers::types::{Address, H256, U256};
+
+    use crate::types::swap_intent_bid::SwapIntentBid;
+
+    use super::*;
+
+    #[test]
+    fn test_calculate_intent_bid_id() {
+        let swap_intent_bid = SwapIntentBid {
+            intent_id: IntentId::from(
+                H256::decode_hex(
+                    "0x76347f79cb00041c374090d5368c061abd8ee93081c67a2318b35f7bb65192a3",
+                )
+                .unwrap(),
+            ),
+            filler: "0xe1AB8145F7E55DC933d51a18c793F901A3A0b276"
+                .parse::<Address>()
+                .unwrap(),
+            fill_amount: U256::from_dec_str("321").unwrap(),
+            intent_bid_id: Default::default(),
+        };
+        let intent_bid: bindings_khalani::base_intent_book::IntentBid = swap_intent_bid.into();
+        let intent_bid_id = calculate_intent_bid_id(intent_bid);
+        let expected_intent_bid_id = IntentId::from(
+            H256::decode_hex("0x98fce71edb803e8602d178e017d0f843fa93cda2223449f4d6c1e2d8b81fd428")
+                .unwrap(),
+        );
+        assert_eq!(expected_intent_bid_id, intent_bid_id);
+    }
 }
